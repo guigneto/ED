cd5f8d24d7e56c6681dcf1ac0ce4d5092a74a3bf
Guilherme Gomes <guilhermeneto26@hotmail.com>
2024-02-27 19:35:27 -0300

    refatorar funcao somatoria para encurtar a funcao main

--- intro-c/Ex1.12.28.c
+++ intro-c/Ex1.12.28.c
@@ -24,23 +24,10 @@
 
  int main(){
     
-    // float numerador, divisao, negativos, positivos;
-    // int denominador;
     float numerador = 1000.0;
     int denominador = 1;
 
-    // for(int i=0;i<50;i++){
-    //     printf("%.0f/%d\n",numerador,denominador);
-    //     divisao = dividir(numerador, denominador); 
-    //     if(denominador%2==0){
-    //         negativos += divisao;
-    //     } else {
-    //         positivos += divisao;
-    //     }
-    //     numerador -= 3;
-    //     denominador++;
-    // }
-
     printf("%.2f",somatoria(numerador, denominador));
+    
     return 0;
  }
