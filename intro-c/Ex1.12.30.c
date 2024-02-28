4f2b729bf70db4fe7db82c6db7b39de2b4817d0b
Guilherme Gomes <guilhermeneto26@hotmail.com>
2024-02-28 16:49:44 -0300

    Adiciona cálculo personalizado do seno e comparação com a função sin()

--- intro-c/Ex1.12.30.c
+++ intro-c/Ex1.12.30.c
@@ -1,5 +1,5 @@
- 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 long int fatorial(int n){
@@ -13,9 +13,38 @@ long int fatorial(int n){
     return fat;
 }
 
+double para_rads(double a){
+  return (a * 3.1415)/180;
+}
+
+double meu_seno(int aGraus, int numTermos){
+  double A = para_rads(aGraus);
+  double senoA = 0;
+  int expoente = 1;
+  int sinal =1;
+
+  for(int i=0;i<numTermos;i++){
+    double fracao = pow(A,expoente)/fatorial(expoente);
+    senoA +=  sinal * fracao;
+    expoente += 2;
+    sinal = sinal * -1;
+  }
+  return senoA;
+}
+
 int main(){
 
-    
+    for(int angulo=0; angulo<=360;angulo++){
+
+      double senoC = sin(para_rads(angulo));
+      double meuSeno =  meu_seno(angulo,10);
+      double diferenca = fabs(senoC - meuSeno); //valor absoluto
+      printf("%3d %10.3lf %10.3lf %10.3lf \n",angulo,senoC,meuSeno,diferenca);
+
+    }
 
     return 0;
 }
+
+// gcc Ex1.12.30.c -lm -o main.bin -Wall
+// -lm = atrelar o binario ja compilado do math.h
