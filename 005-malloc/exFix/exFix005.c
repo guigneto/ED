ee6eb90ff88c1e9e7cc6eacfe80f26759ebee685
Guilherme Gomes <guilhermeneto26@hotmail.com>
2024-06-10 20:44:15 -0300

    feat: Add filtering function to save vehicle data to file

--- 005-malloc/exFix/exFix005.c
+++ 005-malloc/exFix/exFix005.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char placa[12];
@@ -8,10 +9,36 @@ typedef struct {
     int quilometragem;
 } veiculo;
 
+void filtro(veiculo *ptr_veic, char *marca_veiculo) {
+    
+    for (int i = 0; i < 150; i++) {
+        if (strcmp(ptr_veic[i].marca, marca_veiculo) == 0) { // strcmp para comparar strings
+            char nome[50];
+            strcpy(nome, ptr_veic[i].modelo);
+            strcat(nome, ".txt"); // Concatenar sufixo
+            
+            // Criar um path
+            char filepath[100]; 
+            snprintf(filepath, sizeof(filepath), "./txt/%s", nome); //  "./txt/" eh o local desejado
+            FILE *arq = fopen(filepath, "wt");
+            if (arq != NULL) {
+                fprintf(arq, "%s,%s,%s,%d\n", 
+                        ptr_veic[i].placa, 
+                        ptr_veic[i].modelo, 
+                        ptr_veic[i].marca, 
+                        ptr_veic[i].quilometragem);
+                fclose(arq);
+            } else {
+                printf("Erro ao abrir arquivo: %s\n", filepath);
+            }
+        }
+    }
+}
+
 veiculo* loadbdveics(char *nomearq){
 
     veiculo *ptr_veic = malloc(150 * sizeof(veiculo));
-    
+
     FILE * arq = fopen(nomearq,"rt");
 
     for (int i=0;i<150;i++){
@@ -29,14 +56,18 @@ veiculo* loadbdveics(char *nomearq){
 }
 
 int main(void){
-    char nomearq[50] = "./005-malloc/exFix/txt/bdveiculos.txt";
+    char nomearq[50] = "./txt/bdveiculos.txt";
     veiculo *vect = loadbdveics(nomearq);
-    for (int i=0;i,150;i++){
+    
+    for (int i=0;i<150;i++){
         printf("Placa: %s\n",vect[i].placa);
         printf("Modelo: %s\n",vect[i].modelo);
         printf("Marca: %s\n",vect[i].marca);
         printf("Quilometragem: %d\n",vect[i].quilometragem);
     }
+
+    filtro(vect, "MarcaF");
+    
     free(vect);
     return 0;
 }
